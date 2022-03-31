8bab8e28ada48bbd7226fa035fbc09eee0b56bd7
Laura <choidami93@gmail.com>
2022-03-31 21:15:24 +0900

    feat(api-server) : Add Rest API Server module (#12)
    * feat(api-server) : Add Rest API Server module
    
    - api module 추가
    - 간단한 REST API(GET/POST/PUT/DELETE METHOD) 구현
    - GET/POST/PUT/DELETE 테스트 추가
    - root pom.xml 에 api module 추가
    
    * refactor(api-server) : Update annotation
    
    - annotation @RequestMapping -> @GetMapping 형태로 변경

---
+++ api/src/main/java/api/RestApi.java
@@ -0,0 +1,77 @@
+package api;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+@EnableAutoConfiguration
+public class RestApi {
+	private static final Logger logger = LoggerFactory.getLogger(RestApi.class);
+	ArrayList<Parameter> parameters = new ArrayList<>();
+
+	public RestApi() {
+		setParam();
+	}
+
+	@GetMapping()
+	ResponseEntity<?> get() {
+		return ResponseEntity.ok(parameters);
+	}
+
+	@PostMapping("/{id}")
+	ResponseEntity<?> post(@PathVariable("id") String id) {
+		Parameter parameter = new Parameter();
+		parameter.setId(id);
+		parameter.setData(id);
+		parameters.add(parameter);
+
+		return ResponseEntity.ok(parameters);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<?> put(@RequestBody Parameter paramData) {
+		for (Parameter param : parameters) {
+			if (param.getId().equals(paramData.getId())) {
+				param.setData(paramData.getData());
+			}
+		}
+		return ResponseEntity.ok(parameters);
+	}
+
+	@DeleteMapping("/{id}")
+	ResponseEntity<?> delete(@PathVariable String id) {
+		Parameter parameter = new Parameter();
+
+		for (Parameter param : parameters) {
+			if (param.getId().equals(id)) {
+				parameter.setId(id);
+				parameter.setData(param.getData());
+			}
+		}
+
+		parameters.remove(parameter);
+		return ResponseEntity.ok(parameters);
+	}
+
+	void setParam() {
+		for (int i = 0; i < 10; i++) {
+			Parameter parameter = new Parameter();
+			parameter.setId("id" + i);
+			parameter.setData("data" + i);
+			parameters.add(parameter);
+		}
+	}
+}
