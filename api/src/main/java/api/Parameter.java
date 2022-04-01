b57ac2a68e9da2ef94d73e7bdc70436300d80a5e
Laura <choidami93@gmail.com>
2022-04-01 19:05:41 +0900

    Refactor(api-server) : Update Rest Api Server (#13)
    * feat(api-server) : Add Rest API Server module
    
    - api module 추가
    - 간단한 REST API(GET/POST/PUT/DELETE METHOD) 구현
    - GET/POST/PUT/DELETE 테스트 추가
    - root pom.xml 에 api module 추가
    
    * refactor(api-server) : Update annotation
    
    - annotation @RequestMapping -> @GetMapping 형태로 변경
    
    * refactor(api-server) : Update Api Server
    
    - RestApi.java -> RestApiController.java 로 클래스명 변경
    - H2 Database 연동으로 Parameter.java 클래스 Entity로 역할 변경
    - JPA를 이용하여 CRUD 기능 수정

--- api/src/main/java/api/Parameter.java
+++ api/src/main/java/api/Parameter.java
@@ -1,9 +1,42 @@
 package api;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Data
+import lombok.Getter;
+
+@Table(name = "parameter")
+@Entity
+@Getter
 public class Parameter {
+	@Id
+	@Column
 	private String id;
+
+	@Column
 	private String data;
+
+	private Parameter(ParameterBuilder builder) {
+		this.id = builder.id;
+		this.data = builder.data;
+	}
+
+	public Parameter() {
+	}
+
+	public static class ParameterBuilder {
+		private final String id;
+		private final String data;
+
+		public ParameterBuilder(ParameterDto parameterDto) {
+			this.id = parameterDto.getId();
+			this.data = parameterDto.getData();
+		}
+
+		public Parameter build() {
+			return new Parameter(this);
+		}
+	}
 }
---
+++ api/src/main/java/api/ParameterDto.java
@@ -0,0 +1,16 @@
+package api;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ParameterDto {
+	private String id;
+	private String data;
+
+	public ParameterDto(String id, String data) {
+		this.id = id;
+		this.data = data;
+	}
+}
