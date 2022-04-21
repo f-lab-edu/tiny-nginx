7f071878144cf63c37c85449d70c103b14dab85d
serena-git <togoliketheblossoms@gmail.com>
2022-04-21 13:21:23 +0900

    fix(core): simplify code and remove code that calls other servers

--- core/src/main/java/SimpleContentsHandler.java
+++ core/src/main/java/SimpleContentsHandler.java
@@ -1,56 +1,23 @@
-import java.net.InetSocketAddress;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import lombok.extern.slf4j.Slf4j;
 
-public class SimpleContentsHandler extends ChannelInboundHandlerAdapter {
-	Logger logger = LoggerFactory.getLogger(SimpleContentsHandler.class);
-
-
-	private Channel upstreamChannel;
+@Slf4j
+public class SimpleContentsHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
 	@Override
-	public void channelActive(ChannelHandlerContext context) throws Exception {
-
-		// Create a connection for upstream requests
-		Bootstrap bootstrap = new Bootstrap();
-		bootstrap.group(context.channel().eventLoop())
-			.channel(NioSocketChannel.class)
-			.handler(new ChannelInboundHandlerAdapter(){
-				@Override
-				public void channelRead(ChannelHandlerContext ctx, Object message) throws Exception {
-					logger.debug("channelRead() from upstream");
-				}
-			});
-		ChannelFuture channelFuture = bootstrap.connect(new InetSocketAddress(8888));
-		upstreamChannel = channelFuture.channel();
+	protected void channelRead0(ChannelHandlerContext context, FullHttpRequest message) throws Exception {
+		DefaultFullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, message.content().copy());
+		context.writeAndFlush(response);
 	}
 
 	@Override
-	public void channelRead(ChannelHandlerContext context, Object message) throws Exception {
-		logger.debug("channelRead() from client");
-
-		// forward the client request to upstream
-		if(upstreamChannel.isActive()){
-			upstreamChannel.writeAndFlush(message);
-		}
-	}
-
-	@Override
-	public void channelReadComplete(ChannelHandlerContext context) throws Exception {
-		context.flush();
-	}
-
-	@Override
-	public void exceptionCaught(ChannelHandlerContext context, Throwable cause) throws Exception {
-		cause.printStackTrace();
-		context.close();
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.close();
 	}
 }
